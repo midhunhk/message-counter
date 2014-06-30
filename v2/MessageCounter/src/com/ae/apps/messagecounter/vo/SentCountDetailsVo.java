8727523b13f14681d1f7a5dd48e0562e16c4e2a9
midhunhk <midhunhk@gmail.com>
2014-06-30 14:20:20 +0530

    changes for widget, ui tweaks changed settings icon

---
+++ v2/MessageCounter/src/com/ae/apps/messagecounter/vo/SentCountDetailsVo.java
@@ -0,0 +1,48 @@
+package com.ae.apps.messagecounter.vo;
+
+/**
+ * Represents the sent count details
+ * 
+ * @author user
+ * 
+ */
+public class SentCountDetailsVo {
+
+	private int	sentToday;
+	private int	sentCycle;
+	private int	cycleLimit;
+	private int	sentLastCycle;
+
+	public int getSentToday() {
+		return sentToday;
+	}
+
+	public void setSentToday(int sentToday) {
+		this.sentToday = sentToday;
+	}
+
+	public int getSentCycle() {
+		return sentCycle;
+	}
+
+	public void setSentCycle(int sentCycle) {
+		this.sentCycle = sentCycle;
+	}
+
+	public int getCycleLimit() {
+		return cycleLimit;
+	}
+
+	public void setCycleLimit(int cycleLimit) {
+		this.cycleLimit = cycleLimit;
+	}
+
+	public int getSentLastCycle() {
+		return sentLastCycle;
+	}
+
+	public void setSentLastCycle(int sentLastCycle) {
+		this.sentLastCycle = sentLastCycle;
+	}
+
+}
