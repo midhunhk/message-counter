453a63ec4d44820ae24d0247bf29b7c292a8e620
midhunhk <midhunhk@gmail.com>
2017-09-22 01:30:40 +0530

    create IgnoreNumbersManager

--- messageCounter/src/main/java/com/ae/apps/messagecounter/managers/IgnoreNumbersManager.java
+++ messageCounter/src/main/java/com/ae/apps/messagecounter/managers/IgnoreNumbersManager.java
@@ -1,8 +1,27 @@
 package com.ae.apps.messagecounter.managers;
 
-/**
- * Created by user on 9/22/2017.
- */
+import android.content.Context;
 
+import com.ae.apps.messagecounter.db.CounterDataBaseAdapter;
+
+/**
+ * Manages the access to Ignore List table and logic for excluding Ignored numbers from
+ * being counted by the message counter
+ */
 public class IgnoreNumbersManager {
+
+    private final Context mContext;
+
+    /**
+     * Creates an instance of the IgnoreNumbersManager
+     * @param context context
+     */
+    public IgnoreNumbersManager(Context context){
+        mContext = context;
+    }
+
+    public void testMethod(){
+        CounterDataBaseAdapter counterDataBase = CounterDataBaseAdapter.getInstance(mContext);
+    }
+
 }
