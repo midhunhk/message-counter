198129e7bef70e1de720d52a426544930096cf39
Midhun Harikumar <midhunhk@gmail.com>
2018-07-27 16:56:45 -0400

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/ae/apps/messagecounter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ae/apps/messagecounter/ExampleInstrumentedTest.kt
@@ -13,7 +13,7 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+//@RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     //@Test
     fun useAppContext() {
