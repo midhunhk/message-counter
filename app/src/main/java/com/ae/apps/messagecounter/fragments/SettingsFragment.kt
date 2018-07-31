9ee3765deb72e479d78c77b35119a7a19e1b1ff1
midhunhk <midhunhk@gmail.com>
2018-07-31 10:27:28 -0400

    validate message limit preference value

--- app/src/main/java/com/ae/apps/messagecounter/fragments/SettingsFragment.kt
+++ app/src/main/java/com/ae/apps/messagecounter/fragments/SettingsFragment.kt
@@ -2,8 +2,10 @@ package com.ae.apps.messagecounter.fragments
 
 
 import android.os.Bundle
+import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import com.ae.apps.messagecounter.R
+import com.ae.apps.messagecounter.data.preferences.PREF_KEY_MESSAGE_LIMIT_VALUE
 
 
 /**
@@ -18,6 +20,17 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
+
+        // Make sure that only valid numeric value is entered for message limit
+        val messageLimitPref = preferenceScreen.findPreference(PREF_KEY_MESSAGE_LIMIT_VALUE)
+        messageLimitPref.onPreferenceChangeListener = Preference.OnPreferenceChangeListener { _: Preference, newValue: Any ->
+            try {
+                Integer.valueOf(newValue.toString())
+                true
+            } catch (e: NumberFormatException) {
+                false
+            }
+        }
     }
 
 }
