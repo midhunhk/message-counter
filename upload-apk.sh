501289000ebb540ed6d06788073a160bc0bb4501
midhunhk <midhunhk@gmail.com>
2018-08-11 09:55:11 -0400

    update scripts

--- upload-apk.sh
+++ upload-apk.sh
@@ -11,8 +11,8 @@ cp -R app/build/outputs/apk/debugPro/debug/*.apk $HOME/buildApk/
 
 #go to home and git setup  
 cd $HOME
-git config --global user.email "midhunhk@gmail.com"
-git config --global user.name "midhunhk"
+git config --global user.email "noreply@travis.com"
+git config --global user.name "Travis-CI"
 
 # Clone the repository in the folder buildApk
 git clone --quiet --branch ci-builds https://midhunhk:$GITHUB_API_KEY@github.com/midhunhk/message-counter.git ci-builds > /dev/null
@@ -21,11 +21,17 @@ git clone --quiet --branch ci-builds https://midhunhk:$GITHUB_API_KEY@github.com
 cd ci-builds 
 cp -Rf $HOME/buildApk/* ./
 
+#Create a new branch that will contains only latest apk
+git checkout --orphan latest-apk-only
+
 #add, commit and push files
 git add -f . 
-#git remote rm origin 
-#git remote add origin https://midhunhk:$GITHUB_API_KEY@github.com/midhunhk/message-counter.git
-#git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
+
+#delete the ci-builds branch
+git branch -D ci-builds
+#rename the current branch
+git branch -m ci-builds
+
 git push origin ci-builds -fq> /dev/null
-echo -e "Done \ n "
+echo -e "Pushed latest apk \ n "
