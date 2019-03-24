4edfe9201c3adf26f07a45471be8763684b7de82
midhunhk <midhunhk@gmail.com>
2019-03-24 00:31:00 -0400

    more changes

--- app-legacy/src/main/java/com/ae/apps/messagecounter/fragments/MigrateFragment.kt
+++ app-legacy/src/main/java/com/ae/apps/messagecounter/fragments/MigrateFragment.kt
@@ -1,29 +1,31 @@
+/*
+ * Copyright 2019 Midhun Harikumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.ae.apps.messagecounter.fragments
-
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-import com.ae.apps.messagecounter.legacy.debug.R
+import com.ae.apps.messagecounter.R
 
 /**
- * A simple [Fragment] subclass.
- * Use the [MigrateFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
+ * Migration Fragment
  */
 class MigrateFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -31,24 +33,8 @@ class MigrateFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_migrate, container, false)
     }
 
-
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MigrateFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                MigrateFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        fun newInstance() = MigrateFragment()
     }
 }
